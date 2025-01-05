bf43d79913f42d46251bee6417c7bafe7df18ab7
rakshitkapoor <75168728+rakshitkapoor@users.noreply.github.com>
2025-01-05 23:32:44 +0530

    contact list search implemeted

--- lib/features/select_contacts/screens/select_contacts_screen.dart
+++ lib/features/select_contacts/screens/select_contacts_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connectus/colors.dart';
 import 'package:connectus/common/widgets/error.dart';
 import 'package:connectus/common/widgets/loader.dart';
 import 'package:connectus/features/select_contacts/controller/select_contact_controller.dart';
@@ -5,9 +6,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SelectContactsScreen extends ConsumerWidget {
+class SelectContactsScreen extends ConsumerStatefulWidget {
   static const String routeName = '/select-contact';
   const SelectContactsScreen({super.key});
+
+  @override
+  ConsumerState<SelectContactsScreen> createState() =>
+      _SelectContactsScreenState();
+}
+
+class _SelectContactsScreenState extends ConsumerState<SelectContactsScreen> {
+  final TextEditingController searchController = TextEditingController();
+  List<Contact> searchResults = [];
+  bool isSearching = false;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    searchController.dispose();
+  }
+
+  void searhContact(String query, List<Contact> contacts) {
+    if (query.isNotEmpty) {
+      setState(() {
+        isSearching = true;
+        searchResults =
+            contacts
+                .where(
+                  (contacts) => contacts.displayName.toLowerCase().contains(
+                    query.toLowerCase(),
+                  ),
+                )
+                .toList();
+      });
+    } else {
+      setState(() {
+        isSearching = false;
+        searchResults = contacts;
+      });
+    }
+  }
 
   void selectContact(
     WidgetRef ref,
@@ -20,12 +59,46 @@ class SelectContactsScreen extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Select Contact"),
+        title:
+            isSearching
+                ? TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: "Search Contacts...",
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(color: greyColor),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                  onChanged:
+                      (value) => ref
+                          .watch(getContactProvider)
+                          .whenData(
+                            (contacts) => searhContact(value, contacts),
+                          ),
+                )
+                : Text("Select Contact"),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                isSearching = !isSearching;
+                if (!isSearching) {
+                  searchController.clear();
+                  searchResults = [];
+                } else {
+                  ref.read(getContactProvider).whenData((contacts) {
+                    setState(() {
+                      searchResults = contacts;
+                    });
+                  });
+                }
+              });
+            },
+            icon: Icon(Icons.search),
+          ),
           IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
         ],
       ),
@@ -33,33 +106,42 @@ class SelectContactsScreen extends ConsumerWidget {
           .watch(getContactProvider)
           .when(
             data:
-                (contactList) => ListView.builder(
-                  itemCount: contactList.length,
-                  itemBuilder: (context, index) {
-                    final contact = contactList[index];
-                    return InkWell(
-                      onTap: ()=> selectContact(ref,contact,context),
-                      child: Padding(
-                        padding: EdgeInsets.only(bottom: 8.0),
-                        child: ListTile(
-                          title: Text(
-                            contact.displayName,
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                          leading:
-                              contact.photo == null
-                                  ? null
-                                  : CircleAvatar(
-                                    backgroundImage: MemoryImage(
-                                      contact.photo!,
-                                    ),
-                                    radius: 30,
+                (contactList) =>
+                    isSearching && searchResults.isEmpty
+                        ? Container()
+                        : ListView.builder(
+                          itemCount:
+                              isSearching
+                                  ? searchResults.length
+                                  : contactList.length,
+                          itemBuilder: (context, index) {
+                            final contact =
+                                isSearching
+                                    ? searchResults[index]
+                                    : contactList[index];
+                            return InkWell(
+                              onTap: () => selectContact(ref, contact, context),
+                              child: Padding(
+                                padding: EdgeInsets.only(bottom: 8.0),
+                                child: ListTile(
+                                  title: Text(
+                                    contact.displayName,
+                                    style: const TextStyle(fontSize: 18),
                                   ),
+                                  leading:
+                                      contact.photo == null
+                                          ? null
+                                          : CircleAvatar(
+                                            backgroundImage: MemoryImage(
+                                              contact.photo!,
+                                            ),
+                                            radius: 30,
+                                          ),
+                                ),
+                              ),
+                            );
+                          },
                         ),
-                      ),
-                    );
-                  },
-                ),
             error: (err, trace) => ErrorScreen(error: err.toString()),
             loading: () => const Loader(),
           ),
